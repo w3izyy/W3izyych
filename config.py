4b98a87c9a909b7354cfc414d2a388d235774d3e
w3izyy <135378868+w3izyy@users.noreply.github.com>
2023-06-02 22:17:30 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+token = '6216766170:AAH7syvRM4MADxMS4iW0Cz-jLzz_ZCrAS-A' # Токен бота
+loginbot = '@W3izyyBot' # Логин бота телеграм
+
+QIWI_PUBLICKEY = '11111' #p2p public key
+QIWI_TOKEN = '1111111' # токен киви
+QIWI_NUMBER = '77777777777' # Номер киви без +
+
+admin_id = 1120029890 # Ваш айди
+
+price = 0 # Цена за доступ
+
+#Ссылка на канал - @slivmenss
+#Наш чат - @slivmens_chat
